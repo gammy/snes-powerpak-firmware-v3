b2c066e24c29aa4fea1ad7329d2bf192b5dc7db9
gammy <gammy@users.noreply.github.com>
2018-02-14 19:40:56 +0100

    Add snesmod executable to src. Update shellscripts

--- src/makeall.sh
+++ src/makeall.sh
@@ -1,30 +1,36 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+path_out=out/POWERPAK
 
-echo '-- ASSEMBLING SNES POWERPAK SOFTWARE --'
+echo "-- ASSEMBLING SNES POWERPAK SOFTWARE --"
 
-echo '-- generating POWERPAK.CFG --'
-dd if=/dev/zero of=out/POWERPAK/POWERPAK.CFG  bs=512  count=1
+echo "Generating POWERPAK.CFG"
+dd status=none if=/dev/zero of=${path_out}/POWERPAK.CFG  bs=512  count=1
 
-echo '-- generating LASTGAME.LOG --'
-dd if=/dev/zero of=out/POWERPAK/LASTGAME.LOG  bs=512  count=1
+echo "Generating LASTGAME.LOG"
+dd status=none if=/dev/zero of=${path_out}/LASTGAME.LOG  bs=512  count=1
 
-echo '-- generating ERROR.LOG --'
-dd if=/dev/zero of=out/POWERPAK/ERROR.LOG  bs=2K  count=1
+echo "Generating ERROR.LOG"
+dd status=none if=/dev/zero of=${path_out}/ERROR.LOG bs=2K  count=1
 
 #echo -- copying TOPLEVEL.BIT --
 #cp TOPLEVEL.BIT out/POWERPAK/TOPLEVEL.BIT
 
-#echo '-- preparing SOUNDBANK --'
-#smconv -s -o soundbnk "music\parforceritt_b2_downsampled.it"
+echo "Preparing soundbank"
+wine snesmod/smconv.exe -s -o soundbnk "music/parforceritt_b2_downsampled.it"
+mv soundbnk.bank soundbnk.bnk
 
 echo -e '[objects]\nbootrom.o' >> bootrom.lnk
-wla-65816 -x -o bootrom.o bootrom.asm
-wlalink -r -s bootrom.lnk bootrom.sfc
+
+echo "Compiling bootrom"
+wla-65816 -xo bootrom.asm bootrom.o 
+
+echo "Linking bootrom"
+wlalink -rs bootrom.lnk bootrom.sfc
+
 cp bootrom.sfc out/bootrom.sfc
 cp bootrom.sfc out/POWERPAK/UPDATE.ROM
 rm bootrom.lnk
 rm bootrom.o
 
 echo '-- FINISHED! --'
-echo 'Press any key to continue...'
-read -n 1 -s
